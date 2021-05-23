ce9f349bd485b3116d439b00e2ffbab240c2a5ec
taimamuang <84404957+taimamuang@users.noreply.github.com>
2021-05-23 23:30:03 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,57 +5,77 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pwd.h>
 
 #define MAX_LEN_LINE    100
+#define LEN_HOSTNAME    30
 
 int main(void)
 {
     char command[MAX_LEN_LINE];
-    char *args[] = {command, NULL};
+    char* args[] = { command, NULL };
     int ret, status;
     pid_t pid, cpid;
-    
+
     while (true) {
-        char *s;
+        char* s;
         int len;
-        
+
+        char hostname[LEN_HOSTNAME + 1];
+        memset(hostname, 0x00, sizeof(hostname));
+        printf("%s", getpwuid(getuid())->pw_name);
+
+        gethostname(hostname, LEN_HOSTNAME);
+        printf("@%s:", hostname);
+
         printf("MyShell $ ");
         s = fgets(command, MAX_LEN_LINE, stdin);
-        if (s == NULL) {
+
+        if (s == "exit") {
+                exit(0);
+        }
+
+        else if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
         
-        len = strlen(command);
-        printf("%d\n", len);
-        if (command[len - 1] == '\n') {
-            command[len - 1] = '\0'; 
+        else {
+            len = strlen(command);
+            printf("%d\n", len);
+            if (command[len - 1] == '\n') {
+                command[len - 1] = '\0';
+            }
+
+            printf("[%s]\n", command);
+
+            if (strcmp(command, "exit") == 0) {
+                break;
+            }
+
+            pid = fork();
+            if (pid < 0) {
+                fprintf(stderr, "fork failed\n");
+                exit(1);
+                }
+            if (pid != 0) {  /* parent */
+                cpid = waitpid(pid, &status, 0);
+                if (cpid != pid) {
+                    fprintf(stderr, "waitpid failed\n");
+                }
+                printf("Child process terminated\n");
+                if (WIFEXITED(status)) {
+                    printf("Exit status is %d\n", WEXITSTATUS(status));
+                }
+            }
+            else {  /* child */
+                ret = execve(args[0], args, NULL);
+                if (ret < 0) {
+                    fprintf(stderr, "execve failed\n");
+                    return 1;
+                }
+            }
         }
-        
-        printf("[%s]\n", command);
-      
-        pid = fork();
-        if (pid < 0) {
-            fprintf(stderr, "fork failed\n");
-            exit(1);
-        } 
-        if (pid != 0) {  /* parent */
-            cpid = waitpid(pid, &status, 0);
-            if (cpid != pid) {
-                fprintf(stderr, "waitpid failed\n");        
-            }
-            printf("Child process terminated\n");
-            if (WIFEXITED(status)) {
-                printf("Exit status is %d\n", WEXITSTATUS(status)); 
-            }
-        }
-        else {  /* child */
-            ret = execve(args[0], args, NULL);
-            if (ret < 0) {
-                fprintf(stderr, "execve failed\n");   
-                return 1;
-            }
-        } 
     }
     return 0;
 }
